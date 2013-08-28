d77aee05df4e46d32dd14d55992a63d5b20397f5
Scott Oster <scott.oster@inventrio.com>
2013-08-28 13:56:23 -0400

    fixed two bugs in CDS where caGrid 1.4's calls to find delegated credentials seemed to: 1) always be passing in a delegation filter with delegation id (instead of null); had to add an explicit 0 check 2) always be passing in a delegation filter with empty string for identity (instead of null); had to add an explicit "" check

--- cagrid-cds/cagrid-cds-wsrf/src/main/java/org/cagrid/cds/service/wsrf/CDSWSRFImpl.java
+++ cagrid-cds/cagrid-cds-wsrf/src/main/java/org/cagrid/cds/service/wsrf/CDSWSRFImpl.java
@@ -77,19 +77,19 @@ import java.util.List;
 
 public class CDSWSRFImpl extends CredentialDelegationServicePortTypeImpl {
 
-	private final Logger logger;
+    private final Logger logger;
 
-	private final CredentialDelegationService cds;
-	private final ResourceHome resourceHome;
+    private final CredentialDelegationService cds;
+    private final ResourceHome resourceHome;
 
-	@javax.annotation.Resource
-	private WebServiceContext wsContext;
+    @javax.annotation.Resource
+    private WebServiceContext wsContext;
 
-	public CDSWSRFImpl(CredentialDelegationService cds) {
+    public CDSWSRFImpl(CredentialDelegationService cds) {
         this.logger = LoggerFactory.getLogger(getClass());
-		this.cds = cds;
-		this.resourceHome = cds.getResourceHome();
-	}
+        this.cds = cds;
+        this.resourceHome = cds.getResourceHome();
+    }
 
     @Override
     public AddAdminResponse addAdmin(AddAdminRequest parameters) throws PermissionDeniedFaultFaultMessage, CDSInternalFaultFaultMessage {
@@ -106,7 +106,8 @@ public class CDSWSRFImpl extends CredentialDelegationServicePortTypeImpl {
     }
 
     @Override
-    public DeleteDelegatedCredentialResponse deleteDelegatedCredential(DeleteDelegatedCredentialRequest parameters) throws PermissionDeniedFaultFaultMessage, CDSInternalFaultFaultMessage {
+    public DeleteDelegatedCredentialResponse deleteDelegatedCredential(DeleteDelegatedCredentialRequest parameters) throws PermissionDeniedFaultFaultMessage,
+            CDSInternalFaultFaultMessage {
         String message = "deleteDelegatedCredential";
         logger.debug(message);
         try {
@@ -120,7 +121,8 @@ public class CDSWSRFImpl extends CredentialDelegationServicePortTypeImpl {
     }
 
     @Override
-    public ApproveDelegationResponse approveDelegation(ApproveDelegationRequest parameters) throws DelegationFaultFaultMessage, CDSInternalFaultFaultMessage , PermissionDeniedFaultFaultMessage    {
+    public ApproveDelegationResponse approveDelegation(ApproveDelegationRequest parameters) throws DelegationFaultFaultMessage, CDSInternalFaultFaultMessage,
+            PermissionDeniedFaultFaultMessage {
         String message = "approveDelegation";
         logger.debug(message);
         try {
@@ -129,19 +131,20 @@ public class CDSWSRFImpl extends CredentialDelegationServicePortTypeImpl {
             response.setDelegatedCredentialReference(getDelegatedCredentialRefernce(id));
             return response;
         } catch (DelegationException e) {
-        	logger.debug(message, e);
+            logger.debug(message, e);
             throw new DelegationFaultFaultMessage(message, e.getFault());
         } catch (PermissionDeniedException e) {
-        	logger.debug(message, e);
+            logger.debug(message, e);
             throw new PermissionDeniedFaultFaultMessage(message, e.getFault());
         } catch (CDSInternalException e) {
-        	logger.debug(message, e);
+            logger.debug(message, e);
             throw new CDSInternalFaultFaultMessage(message, e.getFault());
         }
     }
 
     @Override
-    public FindDelegatedCredentialsResponse findDelegatedCredentials(FindDelegatedCredentialsRequest parameters) throws PermissionDeniedFaultFaultMessage , CDSInternalFaultFaultMessage    {
+    public FindDelegatedCredentialsResponse findDelegatedCredentials(FindDelegatedCredentialsRequest parameters) throws PermissionDeniedFaultFaultMessage,
+            CDSInternalFaultFaultMessage {
         String message = "findDelegatedCredentials";
         logger.debug(message);
         try {
@@ -157,7 +160,8 @@ public class CDSWSRFImpl extends CredentialDelegationServicePortTypeImpl {
     }
 
     @Override
-    public InitiateDelegationResponse initiateDelegation(InitiateDelegationRequest parameters) throws InvalidPolicyFaultFaultMessage, DelegationFaultFaultMessage , CDSInternalFaultFaultMessage , PermissionDeniedFaultFaultMessage    {
+    public InitiateDelegationResponse initiateDelegation(InitiateDelegationRequest parameters) throws InvalidPolicyFaultFaultMessage,
+            DelegationFaultFaultMessage, CDSInternalFaultFaultMessage, PermissionDeniedFaultFaultMessage {
         String message = "initiateDelegation";
         logger.debug(message);
         try {
@@ -166,27 +170,31 @@ public class CDSWSRFImpl extends CredentialDelegationServicePortTypeImpl {
             response.setDelegationSigningRequest(dsr);
             return response;
         } catch (DelegationException e) {
-        	logger.debug(message, e);
+            logger.debug(message, e);
             throw new DelegationFaultFaultMessage(message, e.getFault());
         } catch (PermissionDeniedException e) {
-        	logger.debug(message, e);
+            logger.debug(message, e);
             throw new PermissionDeniedFaultFaultMessage(message, e.getFault());
         } catch (CDSInternalException e) {
-        	logger.debug(message, e);
+            logger.debug(message, e);
             throw new CDSInternalFaultFaultMessage(message, e.getFault());
         } catch (InvalidPolicyException e) {
-        	logger.debug(message, e);
+            logger.debug(message, e);
             throw new InvalidPolicyFaultFaultMessage(message, e.getFault());
         }
     }
 
     @Override
-    public org.cagrid.cds.wsrf.stubs.FindCredentialsDelegatedToClientResponse findCredentialsDelegatedToClient(FindCredentialsDelegatedToClientRequest parameters) throws PermissionDeniedFaultFaultMessage , CDSInternalFaultFaultMessage    {
+    public org.cagrid.cds.wsrf.stubs.FindCredentialsDelegatedToClientResponse findCredentialsDelegatedToClient(
+            FindCredentialsDelegatedToClientRequest parameters) throws PermissionDeniedFaultFaultMessage, CDSInternalFaultFaultMessage {
         String message = "findCredentialsDelegatedToClient";
         logger.debug(message);
         try {
+            logger.debug("Looking for credentials delegated to client:" + getCallerId() + ", using filter:"
+                    + parameters.getFilter().getClientDelegationFilter().getGridIdentity());
             List<DelegationRecord> records = cds.findCredentialsDelegatedToClient(getCallerId(), parameters.getFilter().getClientDelegationFilter());
             FindCredentialsDelegatedToClientResponse response = new FindCredentialsDelegatedToClientResponse();
+            logger.debug("Found " + records.size() + " records.");
             for (DelegationRecord record : records) {
                 DelegationDescriptor descriptor = new DelegationDescriptor();
                 descriptor.setDelegatedCredentialReference(getDelegatedCredentialRefernce(record.getDelegationIdentifier()));
@@ -195,6 +203,7 @@ public class CDSWSRFImpl extends CredentialDelegationServicePortTypeImpl {
                 descriptor.setIssuedCredentialLifetime(record.getIssuedCredentialLifetime());
                 descriptor.setIssuedCredentialPathLength(record.getIssuedCredentialPathLength());
                 response.getDelegationDescriptor().add(descriptor);
+                logger.debug("Returning record:" + record.getDelegationIdentifier());
             }
             return response;
         } catch (PermissionDeniedException e) {
@@ -205,7 +214,8 @@ public class CDSWSRFImpl extends CredentialDelegationServicePortTypeImpl {
     }
 
     @Override
-    public UpdateDelegatedCredentialStatusResponse updateDelegatedCredentialStatus(UpdateDelegatedCredentialStatusRequest parameters) throws DelegationFaultFaultMessage , CDSInternalFaultFaultMessage , PermissionDeniedFaultFaultMessage    {
+    public UpdateDelegatedCredentialStatusResponse updateDelegatedCredentialStatus(UpdateDelegatedCredentialStatusRequest parameters)
+            throws DelegationFaultFaultMessage, CDSInternalFaultFaultMessage, PermissionDeniedFaultFaultMessage {
         String message = "updateDelegatedCredentialStatus";
         logger.debug(message);
         try {
@@ -221,11 +231,13 @@ public class CDSWSRFImpl extends CredentialDelegationServicePortTypeImpl {
     }
 
     @Override
-    public SearchDelegatedCredentialAuditLogResponse searchDelegatedCredentialAuditLog(SearchDelegatedCredentialAuditLogRequest parameters) throws DelegationFaultFaultMessage , CDSInternalFaultFaultMessage , PermissionDeniedFaultFaultMessage    {
+    public SearchDelegatedCredentialAuditLogResponse searchDelegatedCredentialAuditLog(SearchDelegatedCredentialAuditLogRequest parameters)
+            throws DelegationFaultFaultMessage, CDSInternalFaultFaultMessage, PermissionDeniedFaultFaultMessage {
         String message = "searchDelegatedCredentialAuditLog";
         logger.debug(message);
         try {
-            List<DelegatedCredentialAuditRecord> records = cds.searchDelegatedCredentialAuditLog(getCallerId(), parameters.getF().getDelegatedCredentialAuditFilter());
+            List<DelegatedCredentialAuditRecord> records = cds.searchDelegatedCredentialAuditLog(getCallerId(), parameters.getF()
+                    .getDelegatedCredentialAuditFilter());
             SearchDelegatedCredentialAuditLogResponse response = new SearchDelegatedCredentialAuditLogResponse();
             response.getDelegatedCredentialAuditRecord().addAll(records);
             return response;
@@ -239,7 +251,7 @@ public class CDSWSRFImpl extends CredentialDelegationServicePortTypeImpl {
     }
 
     @Override
-    public RemoveAdminResponse removeAdmin(RemoveAdminRequest parameters) throws PermissionDeniedFaultFaultMessage , CDSInternalFaultFaultMessage    {
+    public RemoveAdminResponse removeAdmin(RemoveAdminRequest parameters) throws PermissionDeniedFaultFaultMessage, CDSInternalFaultFaultMessage {
         String message = "removeAdmin";
         logger.debug(message);
         try {
@@ -253,7 +265,7 @@ public class CDSWSRFImpl extends CredentialDelegationServicePortTypeImpl {
     }
 
     @Override
-    public GetAdminsResponse getAdmins(GetAdminsRequest parameters) throws PermissionDeniedFaultFaultMessage , CDSInternalFaultFaultMessage    {
+    public GetAdminsResponse getAdmins(GetAdminsRequest parameters) throws PermissionDeniedFaultFaultMessage, CDSInternalFaultFaultMessage {
         String message = "getAdmins";
         logger.debug(message);
 
@@ -269,8 +281,7 @@ public class CDSWSRFImpl extends CredentialDelegationServicePortTypeImpl {
     }
 
     @Override
-    public GetResourcePropertyResponse getResourceProperty(QName resourcePropertyQName)
-            throws ResourceUnknownFault, InvalidResourcePropertyQNameFault {
+    public GetResourcePropertyResponse getResourceProperty(QName resourcePropertyQName) throws ResourceUnknownFault, InvalidResourcePropertyQNameFault {
         logger.debug("getResourceProperty " + resourcePropertyQName);
         Exception e = null;
         GetResourcePropertyResponse response = null;
@@ -278,14 +289,11 @@ public class CDSWSRFImpl extends CredentialDelegationServicePortTypeImpl {
             Resource resource = resourceHome.find(null);
             if (resource instanceof ResourcePropertySet) {
                 ResourcePropertySet resourcePropertySet = (ResourcePropertySet) resource;
-                ResourceProperty<?> resourceProperty = resourcePropertySet
-                        .get(resourcePropertyQName);
+                ResourceProperty<?> resourceProperty = resourcePropertySet.get(resourcePropertyQName);
                 if (resourceProperty != null) {
                     Object resourcePropertyValue = resourceProperty.get(0);
-                    logger.debug("getResourceProperty " + resourcePropertyQName
-                            + " returning " + resourcePropertyValue);
-                    resourcePropertyValue = JAXBUtils
-                            .wrap(resourcePropertyValue);
+                    logger.debug("getResourceProperty " + resourcePropertyQName + " returning " + resourcePropertyValue);
+                    resourcePropertyValue = JAXBUtils.wrap(resourcePropertyValue);
                     response = new GetResourcePropertyResponse();
                     response.getAny().add(resourcePropertyValue);
                 }
@@ -298,15 +306,13 @@ public class CDSWSRFImpl extends CredentialDelegationServicePortTypeImpl {
             e = re;
         }
         if ((response == null) || (e != null)) {
-            throw new ResourceUnknownFault("No resource for '"
-                    + resourcePropertyQName + "'", e);
+            throw new ResourceUnknownFault("No resource for '" + resourcePropertyQName + "'", e);
         }
         return response;
     }
 
     @Override
-    public GetServiceSecurityMetadataResponse getServiceSecurityMetadata(
-            GetServiceSecurityMetadataRequest getServiceSecurityMetadataRequest) {
+    public GetServiceSecurityMetadataResponse getServiceSecurityMetadata(GetServiceSecurityMetadataRequest getServiceSecurityMetadataRequest) {
         logger.debug("getServiceSecurityMetadata");
         ServiceSecurityMetadata serviceSecurityMetadata = cds.getServiceSecurityMetadata();
         GetServiceSecurityMetadataResponse response = new GetServiceSecurityMetadataResponse();
@@ -315,10 +321,8 @@ public class CDSWSRFImpl extends CredentialDelegationServicePortTypeImpl {
     }
 
     @Override
-    public QueryResourcePropertiesResponse queryResourceProperties(QueryResourceProperties queryResourcePropertiesRequest)
-            throws QueryEvaluationErrorFault, InvalidQueryExpressionFault,
-            ResourceUnknownFault, InvalidResourcePropertyQNameFault,
-            UnknownQueryExpressionDialectFault {
+    public QueryResourcePropertiesResponse queryResourceProperties(QueryResourceProperties queryResourcePropertiesRequest) throws QueryEvaluationErrorFault,
+            InvalidQueryExpressionFault, ResourceUnknownFault, InvalidResourcePropertyQNameFault, UnknownQueryExpressionDialectFault {
         // TODO
         QueryResourcePropertiesResponse response = null;
         response = new QueryResourcePropertiesResponse();
@@ -326,8 +330,7 @@ public class CDSWSRFImpl extends CredentialDelegationServicePortTypeImpl {
     }
 
     @Override
-    public GetMultipleResourcePropertiesResponse getMultipleResourceProperties(
-            GetMultipleResourceProperties getMultipleResourcePropertiesRequest)
+    public GetMultipleResourcePropertiesResponse getMultipleResourceProperties(GetMultipleResourceProperties getMultipleResourcePropertiesRequest)
             throws ResourceUnknownFault, InvalidResourcePropertyQNameFault {
 
         String message = "getMultipleResourceProperties";
@@ -341,13 +344,12 @@ public class CDSWSRFImpl extends CredentialDelegationServicePortTypeImpl {
         return WebServiceCallerId.getCallerId(wsContext);
     }
 
-    private DelegatedCredentialReference getDelegatedCredentialRefernce(
-            DelegationIdentifier id) throws CDSInternalFaultFaultMessage {
+    private DelegatedCredentialReference getDelegatedCredentialRefernce(DelegationIdentifier id) throws CDSInternalFaultFaultMessage {
 
         try {
-        	MessageContext msgContext = wsContext.getMessageContext();
-        	HttpServletRequest request = (HttpServletRequest) msgContext.get("HTTP.REQUEST");
-        	StringBuffer transportURL = request.getRequestURL();
+            MessageContext msgContext = wsContext.getMessageContext();
+            HttpServletRequest request = (HttpServletRequest) msgContext.get("HTTP.REQUEST");
+            StringBuffer transportURL = request.getRequestURL();
             transportURL.append("/DelegatedCredential");
 
             EndpointReferenceType epr = createEndpointReference(transportURL.toString(), getResourceKey(id));
@@ -360,8 +362,7 @@ public class CDSWSRFImpl extends CredentialDelegationServicePortTypeImpl {
         }
     }
 
-    private EndpointReferenceType createEndpointReference(String address, ResourceKey key)
-            throws Exception {
+    private EndpointReferenceType createEndpointReference(String address, ResourceKey key) throws Exception {
         EndpointReferenceType reference = new EndpointReferenceType();
         if (key != null) {
             ReferencePropertiesType referenceProperties = new ReferencePropertiesType();
@@ -381,10 +382,11 @@ public class CDSWSRFImpl extends CredentialDelegationServicePortTypeImpl {
     }
 
     private ResourceKey getResourceKey(DelegationIdentifier id) throws Exception {
-        //TODO: move this elsewhere common with service bundle
-		ResourceKey key = new SimpleResourceKey(new QName("http://cds.gaards.cagrid.org/CredentialDelegationService/DelegatedCredential", "DelegatedCredentialKey"), id);
-		return key;
-	}
+        // TODO: move this elsewhere common with service bundle
+        ResourceKey key = new SimpleResourceKey(new QName("http://cds.gaards.cagrid.org/CredentialDelegationService/DelegatedCredential",
+                "DelegatedCredentialKey"), id);
+        return key;
+    }
 
     private void setAny(ReferencePropertiesType object, SOAPElement value) {
         if (value == null || object == null) {
