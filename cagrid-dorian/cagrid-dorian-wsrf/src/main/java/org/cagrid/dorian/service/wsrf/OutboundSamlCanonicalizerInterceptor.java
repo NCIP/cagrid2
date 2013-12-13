db9e7709273bff875fc8c71a595746d2d72cf819
Scott Oster <scott.oster@inventrio.com>
2013-12-13 16:30:32 -0500

    optimized interceptor to only run on needed operation

--- cagrid-dorian/cagrid-dorian-wsrf/src/main/java/org/cagrid/dorian/service/wsrf/OutboundSamlCanonicalizerInterceptor.java
+++ cagrid-dorian/cagrid-dorian-wsrf/src/main/java/org/cagrid/dorian/service/wsrf/OutboundSamlCanonicalizerInterceptor.java
@@ -1,14 +1,21 @@
 package org.cagrid.dorian.service.wsrf;
 
+import javax.xml.namespace.QName;
+
 import org.apache.cxf.binding.soap.interceptor.SoapPreProtocolOutInterceptor;
 import org.apache.cxf.message.Message;
 import org.apache.cxf.phase.Phase;
+import org.apache.cxf.service.model.BindingOperationInfo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 
 public class OutboundSamlCanonicalizerInterceptor extends AbstractSamlCanonicalizerInterceptor {
     private static final Logger LOG = LoggerFactory.getLogger(OutboundSamlCanonicalizerInterceptor.class);
+    // It's stupid JAXB doesn't generate constants for these...
+    // TODO: could probably take these in as parameters
+    public static final QName AUTHENTICATE_USER_QNAME = new QName("http://cagrid.nci.nih.gov/Dorian",
+            "authenticateUser");
 
     public OutboundSamlCanonicalizerInterceptor() {
         super(Phase.PRE_STREAM);
@@ -17,9 +24,20 @@ public class OutboundSamlCanonicalizerInterceptor extends AbstractSamlCanonicali
 
     @Override
     protected boolean doesOutboundMessageApply(Message message) {
-        //TODO: change this to look for the messages to apply it to
-        
-        return true;
+        BindingOperationInfo operationInfo = message.getExchange().getBindingOperationInfo();
+        if (operationInfo != null && operationInfo.getName() != null) {
+            if (operationInfo.getName().equals(AUTHENTICATE_USER_QNAME)) {
+                LOG.debug("Matching operation:" + operationInfo.getName());
+                return true;
+            } else {
+                LOG.debug("Skipped operation:" + operationInfo.getName());
+                return false;
+            }
+
+        } else {
+            LOG.warn("Unable to determine whether to apply canonicalization as operation info was null. Ensure the Phase is POST_PROTOCOL, and this is running on the inbound path. Returning false.");
+            return false;
+        }
     }
 
     @Override
