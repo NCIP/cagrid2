466e723aaae3ae67d0f101729728f58adc9198d4
Stephen Langella <steve.langella@gmail.com>
2014-01-13 18:54:44 -0500

    Added syncing to the trust fabric to the OSGI trust service.

--- cagrid-trust/cagrid-trust-service/src/main/java/org/cagrid/trust/service/core/TrustService.java
+++ cagrid-trust/cagrid-trust-service/src/main/java/org/cagrid/trust/service/core/TrustService.java
@@ -1,11 +1,18 @@
 package org.cagrid.trust.service.core;
 
+import java.io.File;
+
+import org.cagrid.core.xml.XMLUtils;
+import org.cagrid.trust.model.SyncDescription;
+import org.cagrid.trust.model.SyncReport;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class TrustService implements org.cagrid.trust.service.TrustService {
 
 	private Synchronizer synchronizer;
+
+	private String syncDescription;
 
 	private Object syncMutex = new Object();
 
@@ -23,10 +30,34 @@ public class TrustService implements org.cagrid.trust.service.TrustService {
 		this.synchronizer = synchronizer;
 	}
 
-	public void syncWithTrustFabric() {
-		synchronized (syncMutex) {
-			long start = System.currentTimeMillis();
+	public String getSyncDescription() {
+		return syncDescription;
+	}
 
+	public void setSyncDescription(String syncDescription) {
+		this.syncDescription = syncDescription;
+	}
+
+	public void syncWithTrustFabric() {
+		long start = System.currentTimeMillis();
+		log.info("Syncing with the trust fabric.....");
+		if (getSynchronizer() != null) {
+			synchronized (syncMutex) {
+				File syncDescriptionFile = null;
+				if (getSyncDescription() != null) {
+					syncDescriptionFile = new File(getSyncDescription());
+					SyncDescription des = (SyncDescription) XMLUtils.fromXMLFile(SyncDescription.class, syncDescriptionFile);
+					SyncReport report = getSynchronizer().sync(des);
+				} else {
+					log.warn("Cannot sync with the trust fabric, no sync description file configured");
+				}
+
+			}
+		} else {
+			log.warn("No synchronizer configured for the trust service.");
 		}
+
+		long end = System.currentTimeMillis();
+		log.info("Successfull synced with the trust fabric in " + (end - start) + " milliseconds.");
 	}
 }
