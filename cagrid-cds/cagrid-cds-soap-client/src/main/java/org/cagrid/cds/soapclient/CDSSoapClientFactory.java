4a90bacfce85cb322435d5159d7dc0ead808c209
Stephen Langella <steve.langella@gmail.com>
2014-02-22 09:38:05 -0500

    Fixed WS Clients

--- cagrid-cds/cagrid-cds-soap-client/src/main/java/org/cagrid/cds/soapclient/CDSSoapClientFactory.java
+++ cagrid-cds/cagrid-cds-soap-client/src/main/java/org/cagrid/cds/soapclient/CDSSoapClientFactory.java
@@ -1,47 +1,68 @@
 package org.cagrid.cds.soapclient;
 
+import org.apache.cxf.Bus;
+import org.apache.cxf.bus.spring.SpringBusFactory;
+import org.apache.cxf.configuration.Configurer;
+import org.apache.cxf.configuration.security.KeyManagersType;
+import org.apache.cxf.configuration.security.KeyStoreType;
+import org.apache.cxf.jaxws.JaxWsProxyFactoryBean;
+import org.cagrid.cds.wsrf.service.CredentialDelegationService;
+import org.cagrid.cds.wsrf.stubs.CredentialDelegationServicePortType;
+import org.cagrid.core.common.security.SSLConfigurer;
+
+import javax.net.ssl.KeyManager;
 import java.io.IOException;
 import java.security.GeneralSecurityException;
 
-import javax.net.ssl.KeyManager;
-import javax.xml.ws.BindingProvider;
-
-import org.apache.cxf.configuration.security.KeyManagersType;
-import org.apache.cxf.configuration.security.KeyStoreType;
-import org.cagrid.cds.wsrf.service.CredentialDelegationService;
-import org.cagrid.cds.wsrf.stubs.CredentialDelegationServicePortType;
-import org.cagrid.core.soapclient.SoapClientFactory;
-
 public class CDSSoapClientFactory {
 
-	public static CredentialDelegationServicePortType createSoapClient(
-			String url) {
-		CredentialDelegationService cds = new CredentialDelegationService();
-		CredentialDelegationServicePortType cdsPort = cds
-				.getCredentialDelegationServicePortTypePort();
-		SoapClientFactory.configureSoapClient((BindingProvider) cdsPort, url);
-		return cdsPort;
-	}
+    public static CredentialDelegationServicePortType createSoapClient(
+            String url) {
+        SpringBusFactory bf = new SpringBusFactory();
+        Bus bus = bf.createBus();
+        JaxWsProxyFactoryBean cf = new JaxWsProxyFactoryBean();
+        cf.setAddress(url);
+        cf.setServiceClass(CredentialDelegationService.class);
+        cf.setBus(bus);
+        CredentialDelegationServicePortType cdsPort = cf.create(CredentialDelegationServicePortType.class);
+        return cdsPort;
+    }
 
-	public static CredentialDelegationServicePortType createSoapClient(
-			String url, KeyStoreType truststore, KeyManagersType keyManager)
-			throws GeneralSecurityException, IOException {
-		CredentialDelegationService cds = new CredentialDelegationService();
-		CredentialDelegationServicePortType cdsPort = cds
-				.getCredentialDelegationServicePortTypePort();
-		SoapClientFactory.configureSoapClient((BindingProvider) cdsPort, url,
-				truststore, keyManager);
-		return cdsPort;
-	}
+    public static CredentialDelegationServicePortType createSoapClient(
+            String url, KeyStoreType truststore, KeyManagersType keyManager)
+            throws GeneralSecurityException, IOException {
+        SpringBusFactory bf = new SpringBusFactory();
+        Bus bus = bf.createBus();
+        Configurer baseConf = bus.getExtension(Configurer.class);
+        SSLConfigurer sslConf = new SSLConfigurer(baseConf);
+        sslConf.setTruststore(truststore);
+        sslConf.setKeystore(keyManager);
+        bus.setExtension(sslConf, Configurer.class);
 
-	public static CredentialDelegationServicePortType createSoapClient(
-			String url, KeyStoreType truststore, KeyManager keyManager)
-			throws GeneralSecurityException, IOException {
-		CredentialDelegationService cds = new CredentialDelegationService();
-		CredentialDelegationServicePortType cdsPort = cds
-				.getCredentialDelegationServicePortTypePort();
-		SoapClientFactory.configureSoapClient((BindingProvider) cdsPort, url,
-				truststore, keyManager);
-		return cdsPort;
-	}
+        JaxWsProxyFactoryBean cf = new JaxWsProxyFactoryBean();
+        cf.setAddress(url);
+        cf.setServiceClass(CredentialDelegationService.class);
+        cf.setBus(bus);
+        CredentialDelegationServicePortType cdsPort = cf.create(CredentialDelegationServicePortType.class);
+        return cdsPort;
+    }
+
+    public static CredentialDelegationServicePortType createSoapClient(
+            String url, KeyStoreType truststore, KeyManager keyManager)
+            throws GeneralSecurityException, IOException {
+        SpringBusFactory bf = new SpringBusFactory();
+        Bus bus = bf.createBus();
+        Configurer baseConf = bus.getExtension(Configurer.class);
+        SSLConfigurer sslConf = new SSLConfigurer(baseConf);
+        sslConf.setTruststore(truststore);
+        sslConf.setKm(new KeyManager[]{keyManager});
+        bus.setExtension(sslConf, Configurer.class);
+
+        JaxWsProxyFactoryBean cf = new JaxWsProxyFactoryBean();
+        cf.setAddress(url);
+        cf.setServiceClass(CredentialDelegationService.class);
+        cf.setBus(bus);
+        CredentialDelegationServicePortType cdsPort = cf.create(CredentialDelegationServicePortType.class);
+        return cdsPort;
+    }
 }
--- cagrid-cds/cagrid-cds-soap-client/src/main/java/org/cagrid/cds/soapclient/DCSSoapClientFactory.java
+++ cagrid-cds/cagrid-cds-soap-client/src/main/java/org/cagrid/cds/soapclient/DCSSoapClientFactory.java
@@ -1,41 +1,74 @@
 package org.cagrid.cds.soapclient;
 
+import org.apache.cxf.Bus;
+import org.apache.cxf.bus.spring.SpringBusFactory;
+import org.apache.cxf.configuration.Configurer;
 import org.apache.cxf.configuration.security.KeyManagersType;
 import org.apache.cxf.configuration.security.KeyStoreType;
-import org.cagrid.core.soapclient.SoapClientFactory;
+import org.apache.cxf.jaxws.JaxWsProxyFactoryBean;
+import org.cagrid.core.common.security.SSLConfigurer;
 import org.cagrid.delegatedcredential.wsrf.service.DelegatedCredentialService;
 import org.cagrid.delegatedcredential.wsrf.stubs.DelegatedCredentialPortType;
 
 import javax.net.ssl.KeyManager;
-import javax.xml.ws.BindingProvider;
 import java.io.IOException;
 import java.security.GeneralSecurityException;
 
 public class DCSSoapClientFactory {
 
-	public static DelegatedCredentialPortType createSoapClient(
-			String url) {
-        DelegatedCredentialService cds = new DelegatedCredentialService();
-        DelegatedCredentialPortType cdsPort = cds.getDelegatedCredentialPortTypePort();
-		SoapClientFactory.configureSoapClient((BindingProvider) cdsPort, url);
-		return cdsPort;
-	}
 
-	public static DelegatedCredentialPortType createSoapClient(
-			String url, KeyStoreType truststore, KeyManagersType keyManager)
-			throws GeneralSecurityException, IOException {
-        DelegatedCredentialService cds = new DelegatedCredentialService();
-        DelegatedCredentialPortType cdsPort = cds.getDelegatedCredentialPortTypePort();
-		SoapClientFactory.configureSoapClient((BindingProvider) cdsPort, url, truststore, keyManager);
-		return cdsPort;
-	}
+    public static DelegatedCredentialPortType createSoapClient(
+            String url) {
 
-	public static DelegatedCredentialPortType createSoapClient(
-			String url, KeyStoreType truststore, KeyManager keyManager)
-			throws GeneralSecurityException, IOException {
-        DelegatedCredentialService cds = new DelegatedCredentialService();
-        DelegatedCredentialPortType cdsPort = cds.getDelegatedCredentialPortTypePort();
-		SoapClientFactory.configureSoapClient((BindingProvider) cdsPort, url, truststore, keyManager);
-		return cdsPort;
-	}
+        SpringBusFactory bf = new SpringBusFactory();
+        Bus bus = bf.createBus();
+
+        JaxWsProxyFactoryBean cf = new JaxWsProxyFactoryBean();
+        cf.setAddress(url);
+        cf.setServiceClass(DelegatedCredentialService.class);
+        cf.setBus(bus);
+        DelegatedCredentialPortType cdsPort = cf.create(DelegatedCredentialPortType.class);
+        return cdsPort;
+    }
+
+    public static DelegatedCredentialPortType createSoapClient(
+            String url, KeyStoreType truststore, KeyManagersType keyManager)
+            throws GeneralSecurityException, IOException {
+
+        SpringBusFactory bf = new SpringBusFactory();
+        Bus bus = bf.createBus();
+        Configurer baseConf = bus.getExtension(Configurer.class);
+        SSLConfigurer sslConf = new SSLConfigurer(baseConf);
+        sslConf.setTruststore(truststore);
+        sslConf.setKeystore(keyManager);
+        bus.setExtension(sslConf, Configurer.class);
+
+        JaxWsProxyFactoryBean cf = new JaxWsProxyFactoryBean();
+        cf.setAddress(url);
+        cf.setServiceClass(DelegatedCredentialService.class);
+        cf.setBus(bus);
+        DelegatedCredentialPortType cdsPort = cf.create(DelegatedCredentialPortType.class);
+        return cdsPort;
+
+    }
+
+    public static DelegatedCredentialPortType createSoapClient(
+            String url, KeyStoreType truststore, KeyManager keyManager)
+            throws GeneralSecurityException, IOException {
+        SpringBusFactory bf = new SpringBusFactory();
+        Bus bus = bf.createBus();
+        Configurer baseConf = bus.getExtension(Configurer.class);
+        SSLConfigurer sslConf = new SSLConfigurer(baseConf);
+        sslConf.setTruststore(truststore);
+        sslConf.setKm(new KeyManager[]{keyManager});
+        bus.setExtension(sslConf, Configurer.class);
+
+        JaxWsProxyFactoryBean cf = new JaxWsProxyFactoryBean();
+        cf.setAddress(url);
+        cf.setServiceClass(DelegatedCredentialService.class);
+        cf.setBus(bus);
+        DelegatedCredentialPortType cdsPort = cf.create(DelegatedCredentialPortType.class);
+        return cdsPort;
+    }
 }
+
