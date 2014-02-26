ef5b6b0cd2ba8c71906ee80754f6a592218faf01
Scott Oster <scott.oster@inventrio.com>
2014-02-26 00:17:06 -0500

    removed unnecessary test as it doesn't actually work on some platforms due to this bug: https://issues.apache.org/bugzilla/show_bug.cgi?id=40897

--- cagrid-dorian/cagrid-dorian-service/src/test/java/org/cagrid/dorian/service/impl/DorianImplTest.java
+++ cagrid-dorian/cagrid-dorian-service/src/test/java/org/cagrid/dorian/service/impl/DorianImplTest.java
@@ -6,7 +6,6 @@ import java.io.ByteArrayInputStream;
 import java.security.KeyPair;
 import java.security.cert.X509Certificate;
 
-import org.cagrid.core.common.JAXBUtils;
 import org.cagrid.dorian.model.federation.CertificateLifetime;
 import org.cagrid.dorian.service.CertificateSignatureAlgorithm;
 import org.cagrid.dorian.service.Dorian;
@@ -17,69 +16,51 @@ import org.cagrid.gaards.saml.encoding.SAMLUtils;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.oasis.names.tc.saml.assertion.AssertionType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.w3c.dom.Element;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "classpath:META-INF/spring/dorian-configuration.xml" })
 public class DorianImplTest {
 
-	@javax.annotation.Resource
-	private Dorian dorian;
+    @javax.annotation.Resource
+    private Dorian dorian;
 
-	@Test
-	public void testDorian() {
-		Assert.assertNotNull(dorian);
-	}
+    @Test
+    public void testDorian() {
+        Assert.assertNotNull(dorian);
+    }
 
+    @Test
+    public void testAuthenticate() throws Exception {
+        BasicAuthentication credential = new BasicAuthentication();
+        credential.setUserId("dorian");
+        credential.setPassword("DorianAdmin$1");
+        AuthenticateUserRequest request = new AuthenticateUserRequest();
+        AuthenticateUserRequest.Credential credential2 = new AuthenticateUserRequest.Credential();
+        credential2.setCredential(credential);
+        request.setCredential(credential2);
+        SAMLAssertion samlAssertion = dorian.authenticate(credential);
 
-	@Test
-	public void testAuthenticate() throws Exception {
-		BasicAuthentication credential = new BasicAuthentication();
-		credential.setUserId("dorian");
-		credential.setPassword("DorianAdmin$1");
-		AuthenticateUserRequest request = new AuthenticateUserRequest();
-		AuthenticateUserRequest.Credential credential2 = new AuthenticateUserRequest.Credential();
-		credential2.setCredential(credential);
-		request.setCredential(credential2);
-		SAMLAssertion samlAssertion = dorian.authenticate(credential);
+        String samlXML = samlAssertion.toString();
+        System.out.println("samlXML");
+        System.out.println(samlXML);
+        System.out.println();
 
-		String samlXML = samlAssertion.toString();
-		System.out.println("samlXML");
-		System.out.println(samlXML);
-		System.out.println();
+        KeyPair keyPair = KeyUtil.generateRSAKeyPair1024();
+        CertificateLifetime lifetime = new CertificateLifetime();
+        lifetime.setHours(6);
 
-		KeyPair keyPair = KeyUtil.generateRSAKeyPair1024();
-		CertificateLifetime lifetime = new CertificateLifetime();
-		lifetime.setHours(6);
+        // Test that SAMLAssertion to String and back is OK.
+        SAMLAssertion _samlAssertionX = new SAMLAssertion(new ByteArrayInputStream(samlXML.getBytes("UTF-8")));
+        SAMLAssertion samlAssertionX = SAMLUtils.canonicalizeSAMLAssertion(_samlAssertionX);
+        Assert.assertNotSame(_samlAssertionX, samlAssertionX);
+        System.out.println("samlXML canonicalized");
+        System.out.println(samlAssertionX);
+        System.out.println();
+        X509Certificate certificate = dorian.requestUserCertificate(samlAssertionX, keyPair.getPublic(), lifetime,
+                CertificateSignatureAlgorithm.SHA2);
+        Assert.assertNotNull(certificate);
 
-		// Test that SAMLAssertion to String and back is OK.
-		SAMLAssertion _samlAssertionX = new SAMLAssertion(new ByteArrayInputStream(samlXML.getBytes("UTF-8")));
-		SAMLAssertion samlAssertionX = SAMLUtils.canonicalizeSAMLAssertion(_samlAssertionX);
-		Assert.assertNotSame(_samlAssertionX, samlAssertionX);
-		System.out.println("samlXML canonicalized");
-		System.out.println(samlAssertionX);
-		System.out.println();
-		X509Certificate certificate = dorian.requestUserCertificate(samlAssertionX, keyPair.getPublic(), lifetime, CertificateSignatureAlgorithm.SHA2);
-		Assert.assertNotNull(certificate);
-
-		// Test round-trip through AssertionType
-		AssertionType assertion = JAXBUtils.unmarshal(AssertionType.class, samlXML);
-		Assert.assertNotNull(assertion);
-		String assertionXML = JAXBUtils.marshal(assertion, SAMLUtils.ASSERTION_QNAME);
-		System.out.println("assertionXML");
-		System.out.println(assertionXML);
-		System.out.println();
-		Element assertionElement = JAXBUtils.marshalToElement(assertion, SAMLUtils.ASSERTION_QNAME);
-		_samlAssertionX = new SAMLAssertion(assertionElement);
-		samlAssertionX = SAMLUtils.canonicalizeSAMLAssertion(_samlAssertionX);
-		Assert.assertNotSame(_samlAssertionX, samlAssertionX);
-		System.out.println("samlXML canonicalized via Assertion");
-		System.out.println(samlAssertionX);
-		System.out.println();
-		certificate = dorian.requestUserCertificate(samlAssertionX, keyPair.getPublic(), lifetime, CertificateSignatureAlgorithm.SHA2);
-		Assert.assertNotNull(certificate);
-	}
+    }
 }
